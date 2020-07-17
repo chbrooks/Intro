b62d98fc81af31dda3f87bfdc4b328ba19ac5b39
Chris Brooks <chris.cbrooks@gmail.com>
2020-07-17 14:22:16 -0700

    Initial files

---
+++ src/CountChars.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/* Modify to:
+   1. Count vowels.
+   2. determine whether the string contains any punctuation characters.
+   3. Replace with '*'
+ */
+
+public class CountChars {
+    public static void main(String[] args) {
+        String str;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter a string:");
+        str = scanner.nextLine();
+        System.out.println(str.length());
+
+    }
+}
